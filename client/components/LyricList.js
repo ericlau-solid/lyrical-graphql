e8f81d11d8215dce32dbd63c82928eafe84d21f7
Eric Lau <eric.lau@adaptechsolutions.ca>
2017-05-20 02:35:12 -0700

    to lecture 75, example of optimistic ui update made easy with apollo

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -3,11 +3,19 @@ import { graphql } from 'react-apollo';
 import { likeLyric } from '../queries';
 
 class LyricList extends Component {
-  onLike = async (id) => {
+  onLike = async (id, likes) => {
     await this.props.mutate({
       variables: {
         id
-      }
+      },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likeLyric: {
+          id,
+          __typename: 'LyricType',
+          likes: likes + 1
+        }
+      },
     });
   }
 
@@ -22,7 +30,7 @@ class LyricList extends Component {
           {likes}
           <i
             className="material-icons"
-            onClick={() => this.onLike(id)}
+            onClick={() => this.onLike(id, likes)}
           >
             thumb_up
           </i>
